af7f87396d9dc2333da1de20078abdfed14113c3
entertotheneworld <tristan.greffe@hotmail.com>
2022-04-13 16:09:13 +0200

    Ajout commentaire exercice ascenseur

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -2,9 +2,7 @@ def game
     win = 0
     number_turns = 0
     while win < 10
-
         roll_dice = rand(1..6)
-        puts roll_dice
         if roll_dice == 5 || roll_dice == 6
             win += 1
             puts "Tu est à la marche numéro #{win}"
@@ -14,6 +12,8 @@ def game
         else
             puts "Tu est à la marche numéro #{win}"
         end
+
+        # Incrémente le nombre de tour
         number_turns += 1
     end
     
@@ -22,14 +22,17 @@ def game
 end
 
 def average_finish_time
+    # Stock le nb de tour par jeux dans le tableau tab
     tab = []
     for i in 1..100
         tab << game
     end
+
+    # Calcule du nombre total de tour
     total_turns = 0
-    tab.each {|i|
-        total_turns += i
-    }
+    tab.each {|i| total_turns += i}
+
+    # Calcule de la moyenne
     average_turns = total_turns / 100
     puts "Le nombre moyen de tour est de #{average_turns}"
 end
